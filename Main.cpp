5c7eb17a1c4bddafa2e084eca9ccddcb9795a0d5
Tom Petrangelo <tdpetrangelo@gmail.com>
2019-08-12 19:41:26 -0400

    added checks for valid input for integers and doubles, added some comments

--- Main.cpp
+++ Main.cpp
@@ -20,7 +20,7 @@ int main()
 	double arrayDouble[5];
 
 	//initialization for a user-input int and double value
-	int intValue{ 0 };
+	double intValue{ 0.0 };
 	double doubleValue{ 0.0 };
 
 	//for loop that allows user to input integers for arrayInt 
@@ -28,7 +28,23 @@ int main()
 
 		//output asking user to input an element into their array
 		std::cout << "Please enter element " << i + 1 << " for your integer array: ";
-		std::cin >> arrayInt[i];
+		std::cin >> intValue;
+
+		/*the floor of intValue should be the same number if it is an int, if not then number is not an int.
+		ask user to re-input value*/
+		while(floor(intValue)-intValue != 0){
+			std::cout << "Invalid input, please enter an integer: ";
+			
+			//repair the instream
+			std::cin.clear();
+
+			//clear the buffer
+			std::cin.ignore();
+			std::cin >> intValue;
+		}
+
+		//current iteration of i will save intValue into arrayInt
+		arrayInt[i] = intValue;
 	}
 
 	std::cout << std::endl;
@@ -38,13 +54,28 @@ int main()
 
 		//output asking user to input an element into their array
 		std::cout << "Please enter element " << i + 1 << " for your double array: ";
-		std::cin >> arrayDouble[i];
+		std::cin >> doubleValue;
+
+		/*if the floor and ceiling of the input is equal, we know that the input value must not be a double
+		ask the user to re-input, looking for a double value.*/
+		while (floor(doubleValue) == ceil(doubleValue)) {
+			std::cout << "Invalid input, please enter a double: ";
+
+			//repair the instream
+			std::cin.clear();
+
+			//clear the buffer
+			std::cin.ignore();
+			std::cin >> doubleValue;
+		}
+
+		arrayDouble[i] = doubleValue;
 
 	}
 	
 		//outputs for a user-entered integer and double array
-		std::cout << std::endl << "The average of your integer input is: " << array1.average(arrayInt, arraySize) << std::endl;
-		std::cout << std::endl << "The average of your double input is: " << array1.average(arrayDouble, arraySize) << std::endl;
+		std::cout << std::endl << "The average of your integer inputs are: " << array1.average(arrayInt, arraySize) << std::endl;
+		std::cout << std::endl << "The average of your double inputs are: " << array1.average(arrayDouble, arraySize) << std::endl;
 
 	return 0;
 }
